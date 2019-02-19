97268416fd8d73a92d5ed3c53f6317b7d0da899c
RU27 <riddickv_keo@list.ru>
2019-02-19 10:06:11 +1000

    Create rm_message.sh

---
+++ rm_message.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# rm_message.sh <FROM>
+if [ -z "$1" ]; then
+echo "usage:  rm_message.sh <FROM>"
+exit 0
+else
+addr=( mailtemp1@gmail.com anothermail@yahoo.com thirdmailtemp02@gmail.com )
+subject=$1
+#echo "Searching mail $addr from Mail:  $subject"
+for victim in "${addr[@]}";
+  do
+        echo "=======Looking for "$victim""
+        #instead "From:$subject" you can use "subject:$subject" or something another
+        for msg in `zmmailbox -z -m "$victim" s -l 500 -t message "From:$subject"|awk '{ if (NR!=1) {print}}' | grep -v -e Id -e "-" -e "^$" | awk '{ print $2 }'`
+          do
+        echo "Found "$msg""
+        zmmailbox -z -m $victim dm $msg
+        done
+done
+fi
